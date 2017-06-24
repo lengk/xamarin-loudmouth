170901399b4e276d20c9f7050dbede191bb41f8b
Harry <himion0@gmail.com>
2017-06-24 12:48:21 +0100

    Basic API call layout

--- LoudMouth/Controllers/NetworkController.cs
+++ LoudMouth/Controllers/NetworkController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using LoudMouth.Models;
+using Newtonsoft.Json.Linq;
 
 namespace LoudMouth {
     public class NetworkController {
-        string BaseUrl = "https://westus.api.cognitive.microsoft.com/spid/v1.0/"; 
+        string BaseUrl = "https://westus.api.cognitive.microsoft.com/spid/v1.0/";
         string EnrollProfileURL = "identificationProfiles/{0}/enroll?shortAudio={1}"; // POST
         string DeleteProfileURL = "identificationProfiles/{0}"; // DELETE
         string CreateProfileURL = "identifictionProfiles"; // POST
@@ -14,32 +17,67 @@ namespace LoudMouth {
 
         HttpClient client;
 
+        const string SUBSCRIPTION_KEY = "";
+
+        MediaTypeHeaderValue octetMedia = new MediaTypeHeaderValue("application/octet");
+        MediaTypeHeaderValue jsonMedia = new MediaTypeHeaderValue("application/json");
 
         public NetworkController() {
             client = new HttpClient();
             client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SUBSCRIPTION_KEY);
         }
 
 
-        public void CreateProfile(Attendee person) {
-            //client.
+        // Post
+        public async void CreateProfileFor(Attendee person) {
+            Debug.WriteLine("Creating a Profile");
+            var data = new JObject();
+            data.Add("locale", "en-us");
+            var content = new StringContent(data.ToString(), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(CreateProfileURL, content);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(body);
+            person.ProfileID = (string) json.GetValue("asd");
+            Debug.WriteLine(new JObject(body).ToString(Newtonsoft.Json.Formatting.Indented));
         }
 
-        public async void IdentifyProfile(){
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+        // POST 
+        public async void EnrolProfile(Attendee person, bool shortAudio = false) {
+            Debug.WriteLine("Enrol a Profile");
+            var url = string.Format(EnrollProfileURL, person.ProfileID, shortAudio);
+        }
 
+        // POST
+        public async void IdentifyProfile(Attendee[] person, bool shortAudio = false) {
             // Request parameters
-            let uri = string.Format()
+            var url = string.Format(IndentifyProfileURL, shortAudio);
+
             HttpResponseMessage response;
 
             // Request body
-            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+            byte[] byteData = Encoding.UTF8.GetBytes("");
 
             using (var content = new ByteArrayContent(byteData)) {
-                content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
-                response = await client.PostAsync(uri, content);
+                content.Headers.ContentType = octetMedia;
+                response = await client.PostAsync(url, content);
             }
+        }
 
+        // DELETE
+        public async Task<bool> DeleteProfile(Attendee person) {
+            Debug.WriteLine("Delete a Profile");
+            var url = string.Format(DeleteProfileURL, person.ProfileID);
+            var response = await client.DeleteAsync(url);
+            if (response.IsSuccessStatusCode) {
+                Debug.WriteLine("Deleted user");
+                return true;
+            } else {
+                var body = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine("MS API Error: \n" + body);
+                return false;
+            }
         }
     }
 }
